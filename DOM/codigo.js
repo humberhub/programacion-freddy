9f2484c92c7d07deb11ae8b58d484f5da9e70e12
Humberto uribe <HumberDJ@MacBook-Pro-Huj.local>
2020-05-28 18:56:10 -0500

    agregue uns consultas del navegador

--- DOM/codigo.js
+++ DOM/codigo.js
@@ -2,3 +2,8 @@
 var ruta = window.location;
 console.log(ruta);
 document.write("Esta en : " + ruta);
+//estos son una consultas en la consola para las 3 principales partes
+//del navegador 
+console.log(navigator);
+console.log(window);
+console.log(document);
