9730e4fad7b12477248d6fa873cb2185ff8585b2
Humberto uribe <HumberDJ@MacBook-Pro-Huj.local>
2020-05-28 20:22:58 -0500

    dibujar una linea con canvas

---
+++ canvas/dibujo.js
@@ -0,0 +1,12 @@
+
+alert("si funciona");
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+console.log(lienzo);
+
+lienzo.beginPath();
+lienzo.strokeStyle = "blue";
+lienzo.moveTo(50,10);
+lienzo.lineTo(250,2);
+lienzo.stroke();
+lienzo.closePath();
