97fbeb42dc56ed4fcff03d05f181484a87990aa2
Humberto uribe <HumberDJ@MacBook-Pro-Huj.local>
2020-05-29 13:06:06 -0500

    dibuje un cuadro con cruz y x

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,5 +1,5 @@
 
-alert("si funciona");
+
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 console.log(lienzo);
@@ -14,5 +14,11 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
   lienzo.closePath();
 }
 
-dibujarLinea("pink",1,300,300,1);
-dibujarLinea("black",1,1,300,300);
+dibujarLinea("#FE9A2E",0,0,300,300);
+dibujarLinea("#FE9A2E",0,300,300,0);
+dibujarLinea("#FE9A2E",1,1,300,1);
+dibujarLinea("#FE9A2E",299,0,299,299);
+dibujarLinea("#FE9A2E",299,299,0,299);
+dibujarLinea("#FE9A2E",1,299,1,1);
+dibujarLinea("#FE9A2E",150,0,150,300);
+dibujarLinea("#FE9A2E",0,150,300,150);
