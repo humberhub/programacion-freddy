36a88c84adb9c96fa46b0a3a2873b5696203b41e
Humberto uribe <HumberDJ@MacBook-Pro-Huj.local>
2020-05-29 23:08:28 -0500

    clase de eventos y formularios en html y javascript

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,23 +1,11 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
 var d = document.getElementById("dibujito");
+var ancho =d.width;
 var lienzo = d.getContext("2d");
 console.log(lienzo);
-var lineas = 30;
-var l = 0;
-var lineas2 = 30;
-var l2 = 0;
-var colorcito = "#FE9A23";
-var yi, xf;
-var xi, yf;
-//ciclo while original del lado izq el dibujo
-while(l<lineas)
-{
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-
-dibujarLinea(colorcito, 0, yi, xf, 300);
-console.log("linea" + l);
-l = l + 1;
-}
 
 //ciclo for
 // //for(l = 0; l<lineas; l++)
@@ -30,17 +18,15 @@ l = l + 1;
 // }
 
 //ciclo do while
-do{
-  xi = 10 * l2;
-  yf = 10 * (l2+1);
-  dibujarLinea(colorcito, xi, 0, 300, yf);
-  l2 = l2 + 1;
-  console.log ("l2 " + l2);
-}
-while(l2< lineas2);
+// do{
+//   xi = 10 * l2;
+//   yf = 10 * (l2+1);
+//   dibujarLinea(colorcito, xi, 0, 300, yf);
+//   l2 = l2 + 1;
+//   console.log ("l2 " + l2);
+// }
+// while(l2< lineas2);
 
-dibujarLinea (colorcito, 1, 0, 1, 300);
-dibujarLinea (colorcito, 0, 299, 300, 299);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -50,4 +36,31 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
   lienzo.lineTo(xfinal, yfinal);
   lienzo.stroke();
   lienzo.closePath();
+}
+
+function dibujoPorClick()
+{
+ var lineas = parseInt(texto.value);
+ alert(lineas);
+ var l = 0;
+ var lineas2 = 30;
+ var l2 = 0;
+ var colorcito = "#FE9A23";
+ var yi, xf;
+ var xi, yf;
+ var espacio = ancho / lineas;
+ //ciclo while original del lado izq el dibujo
+ while(l<lineas)
+ {
+   yi = espacio * l;
+   xf = espacio * (l + 1);
+
+ dibujarLinea(colorcito, 0, yi, xf, 300);
+ console.log("linea" + l);
+ l = l + 1;
+ }
+ dibujarLinea (colorcito, 1, 0, 1, ancho-1);
+ dibujarLinea (colorcito, 0, ancho-1, ancho-1, ancho-1);
+
+
 }
